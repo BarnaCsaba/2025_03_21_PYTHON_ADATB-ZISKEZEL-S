d5f434ffed9b554d6aafb7b21a3041c0fc04b94e
BarnaCsaba_PG <barna.csaba@diak.szbi-pg.hu>
2025-03-28 10:13:57 +0100

    Create my_database.py

---
+++ my_database.py
@@ -0,0 +1,16 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password=""
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("CREATE DATABASE mydatabase")
+
+mycursor.execute("SHOW DATABASES")
+
+for x in mycursor:
+  print(x)
